7960dd4da0448cc7b0fcbed56f4b555c8e513759
Chirani Rajapaksha <chirani.rajapaksha.97@gmail.com>
2026-01-30 20:37:56 +0530

    Implement activity posting

--- activityservice/src/main/java/com/fitness/activityservice/ActivityserviceApplication.java
+++ activityservice/src/main/java/com/fitness/activityservice/ActivityserviceApplication.java
@@ -2,12 +2,13 @@ package com.fitness.activityservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 public class ActivityserviceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ActivityserviceApplication.class, args);
-	}
 
+        SpringApplication.run(ActivityserviceApplication.class, args);
+	}
 }
---
+++ activityservice/src/main/java/com/fitness/activityservice/config/MongoConfig.java
@@ -0,0 +1,37 @@
+package com.fitness.activityservice.config;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableMongoAuditing
+@EnableMongoRepositories(basePackages = "com.fitness.activityservice.repository")
+public class MongoConfig extends AbstractMongoClientConfiguration {
+
+    @Value("${spring.data.mongodb.database:fitness_activity_db}")
+    private String databaseName;
+
+    @Value("${spring.data.mongodb.uri:mongodb://localhost:27017/fitness_activity_db}")
+    private String mongoUri;
+
+    @Override
+    protected String getDatabaseName() {
+        return databaseName;
+    }
+
+    @Override
+    public MongoClient mongoClient() {
+        ConnectionString connectionString = new ConnectionString(mongoUri);
+        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+        return MongoClients.create(mongoClientSettings);
+    }
+}
